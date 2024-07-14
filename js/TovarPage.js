4e07e1b3ffbac97f298c3475043bb0022fc68c11
ilyapavlenkoweb <127994091+RoboLox344@users.noreply.github.com>
2024-07-14 20:41:53 +0300

    Add files via upload

---
+++ js/TovarPage.js
@@ -0,0 +1,63 @@
+const TabsCategori = document.querySelectorAll('.tabs-pointer');
+console.log(TabsCategori);
+TabsCategori.forEach(({id}) => {
+    let Stroka = document.getElementById(id);
+    const ContentList = document.querySelectorAll('.tabs-panel');
+    Stroka.addEventListener('click', function(){
+            if(id == 'Char'){
+                Stroka.classList.add('tabs-acitve');
+                TabsCategori.forEach(({id}) => {
+                    if (id !== 'Char'){
+                        console.log(id)
+                        let DeleteStorka = document.getElementById(id);
+                        DeleteStorka.classList.remove('tabs-acitve')
+                    }
+                });
+                ContentList.forEach(({id}) => {
+                    let pageCont = document.getElementById(id);
+                    if( id == 'CharCont'){ 
+                        pageCont.classList.add('tabs-panel--active');
+                        document.getElementById('OpisContent').classList.remove('tabs-panel--active');
+                        document.getElementById('OtziviContent').classList.remove('tabs-panel--active');
+                    } 
+                });
+            }else if (id == 'Opisanie'){
+                Stroka.classList.add('tabs-acitve');
+                TabsCategori.forEach(({id}) => {
+                    if (id !== 'Opisanie'){
+                        console.log(id)
+                        let DeleteStorka = document.getElementById(id);
+                        DeleteStorka.classList.remove('tabs-acitve')
+                    }
+                });
+                ContentList.forEach(({id}) => {
+                    let pageCont = document.getElementById(id);
+                    if(id == 'OpisContent'){
+                        pageCont.classList.add('tabs-panel--active');
+                        document.getElementById('CharCont').classList.remove('tabs-panel--active');
+                        document.getElementById('OtziviContent').classList.remove('tabs-panel--active');
+                    }
+                });
+            }else if (id == 'Otzivi'){
+                Stroka.classList.add('tabs-acitve');
+                TabsCategori.forEach(({id}) => {
+                    if (id !== 'Otzivi'){
+                        console.log(id)
+                        let DeleteStorka = document.getElementById(id);
+                        DeleteStorka.classList.remove('tabs-acitve')
+                    }
+                });
+                ContentList.forEach(({id}) => {
+                    let pageCont = document.getElementById(id);
+                    if(id == 'OtziviContent'){
+                        pageCont.classList.add('tabs-panel--active');
+                        document.getElementById('CharCont').classList.remove('tabs-panel--active');
+                        document.getElementById('OpisContent').classList.remove('tabs-panel--active');
+                    }
+                });
+            }
+
+        
+    });
+    
+ });
